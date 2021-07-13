70761f0e663d1a66575cf85be25dda635098873d
Rowan McKereghan <rowanmckereghan@gmail.com>
2021-07-12 18:26:53 -0700

    photo update

--- rowanApp/m_and_k_explained/src/frontpage.js
+++ rowanApp/m_and_k_explained/src/frontpage.js
@@ -6,9 +6,7 @@ import { Parallax, ParallaxBanner } from 'react-scroll-parallax';
 
 
 
-const ParallaxB = () => (
-    <ParallaxBanner className="parallax_banner" layers={[{image: "https://cdn.pixabay.com/photo/2017/08/27/03/24/backround-2685094_1280.jpg", amount: 0.3}]}></ParallaxBanner>
-);
+
 
 
 class FrontPage extends Component {
@@ -17,10 +15,9 @@ class FrontPage extends Component {
         return (
             <div id="root">
                 <div id="container">
-                    <ParallaxB/>
-                    <ParallaxB/>
-                    <ParallaxB/>
-                    <ParallaxB/>
+                    <ParallaxBanner className="parallax_banner" layers={[{image: "https://github.com/cu-mkp/sandbox/blob/RowanWebApp/rowanApp/pics%20for%20site/website%20backdrop.png?raw=true", amount: 0.3}]} style={{width: '100%', height: '200px'}}></ParallaxBanner>
+                    <ParallaxBanner className="parallax_banner" layers={[{image: "https://github.com/cu-mkp/sandbox/blob/RowanWebApp/rowanApp/pics%20for%20site/website%20backdrop.png?raw=true", amount: 0.3}]}></ParallaxBanner>
+                    <ParallaxBanner className="parallax_banner" layers={[{image: "https://github.com/cu-mkp/sandbox/blob/RowanWebApp/rowanApp/pics%20for%20site/website%20backdrop.png?raw=true", amount: 0.3}]}></ParallaxBanner>
                 </div>            
             </div>
         );
