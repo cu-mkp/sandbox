1a40f0bb40d1ef8119b7619f1814c96ab54e614b
Rowan McKereghan <rowanmckereghan@gmail.com>
2021-08-02 17:24:37 -0700

    circle testing

--- rowanApp/m_and_k_explained/src/manuscriptpage.js
+++ rowanApp/m_and_k_explained/src/manuscriptpage.js
@@ -1,8 +1,27 @@
 import {React, Component } from "react";
+import './manuscriptpage.css';
+
+
+const IMG_URI = 'https://cdn.pixabay.com/photo/2020/05/26/15/42/eagle-5223559_960_720.jpg';
 
 
 
 class ManuscriptPage extends Component {
+
+
+    render() {
+        return (
+           <div>
+                <div class="circle">
+                    <img src={IMG_URI} alt="Image" /> 
+                </div>
+                <div>&nbsp;</div>
+                <div class="circle">
+                    <img src={IMG_URI} class="landscape" alt="Image" /> 
+                </div>
+            </div>
+        );
+    }
     
 }
 
