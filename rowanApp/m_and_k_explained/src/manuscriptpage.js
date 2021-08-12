348c450aaf1bfba376a4bd22f290466f35e4c736
Rowan McKereghan <rowanmckereghan@gmail.com>
2021-08-12 00:13:19 -0700

    Created basic principle of manuscript page as well as other subpages
    click on a circle to bring up a pop up screen with paragraph. Eac h circle will have a different image to represent the question on it. Each pop up will have a paragraph relating to said question.

--- rowanApp/m_and_k_explained/src/manuscriptpage.js
+++ rowanApp/m_and_k_explained/src/manuscriptpage.js
@@ -1,8 +1,7 @@
 import { React, Component } from "react";
 import './manuscriptpage.css';
 
-
-const IMG_URI = 'https://github.com/cu-mkp/sandbox/blob/RowanWebApp/rowanApp/pics%20for%20site/book%20pic.png?raw=true';
+const IMG_URI_1 = 'https://github.com/cu-mkp/sandbox/blob/RowanWebApp/rowanApp/pics%20for%20site/book%20pic.png?raw=true';
 
 
 
@@ -14,49 +13,65 @@ class ManuscriptPage extends Component {
             clicked: false,
             popUpNum: 0
         }
-        this.popUp0 = this.popUp0.bind(this);
+        this.popUp = this.popUp.bind(this);
     }
 
 
-    popUp0() {
+    popUp() {
+        let str = <p></p>;
+        switch(this.state.popUpNum)
+        {
+            case 0:
+                str = <p>
+                    Lorem Ipsum is simply dummy text of the printing and 
+                    typesetting industry. Lorem Ipsum has been the industry's 
+                    standard dummy text ever since the 1500s, when an unknown 
+                    printer took a galley of type and scrambled it to make a type 
+                    specimen book. It has survived not only five centuries, but also 
+                    the leap into electronic typesetting, remaining essentially 
+                    unchanged. It was popularised in the 1960s with the release of 
+                    Letraset sheets containing Lorem Ipsum passages, and more 
+                    recently with desktop publishing software like Aldus PageMaker 
+                    including versions of Lorem Ipsum.
+                </p>
+                break;
+            case 1:
+        }
         return(
             <div className={this.state.clicked ? "popup" : "popuphidden"} id="popupnum0">
                 <button className="popUpClose" onClick={() => this.closePopUp()}>⬅</button>
+                <h2 className="popupheader">What is Ms. Fr. 640?</h2>
                 <br></br>
                 <br></br>
-                Lorem Ipsum is simply dummy text of the printing and 
-                typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s, when an unknown 
-                printer took a galley of type and scrambled it to make a type 
-                specimen book. It has survived not only five centuries, but also 
-                the leap into electronic typesetting, remaining essentially 
-                unchanged. It was popularised in the 1960s with the release of 
-                Letraset sheets containing Lorem Ipsum passages, and more 
-                recently with desktop publishing software like Aldus PageMaker 
-                including versions of Lorem Ipsum.
+                <div className="popupcontent">{str}</div>
+                <div className="popupcontent">{str}</div>
             </div>
         );
     }
 
     closePopUp = () => {
-        this.setState({
-            clicked: false
-        })
         const animationender = document.getElementById("popupnum0");
-        animationender.addEventListener('animationend', () => document.getElementById('root').style.setProperty("--popupdisplay", "none"));
-        console.log(animationender.onanimationend);
+        animationender.addEventListener('animationend', this.tempfunc);
+        this.setState({
+            clicked: false,
+            listener: true
+        })
     }
 
+    tempfunc = () => {
+        document.getElementById('root').style.setProperty("--popupdisplay", "none")
+        console.log('changing css var')
+    }
+
+
     handleClick = (circleNum) => {
+        document.getElementById('root').style.setProperty("--popupdisplay", "block");
+        const animationender = document.getElementById("popupnum0");
+        animationender.removeEventListener('animationend', this.tempfunc);
         this.setState({
             popUpNum: circleNum,
             clicked: true
         });
-        //const animationender = document.getElementById("popupnum0");
-        document.getElementById('root').style.setProperty("--popupdisplay", "block");
-        /*animationender.style.animation = 'none';
-        animationender.scrollBy(); 
-        animationender.style.animation = null; */
     }
 
 
@@ -67,14 +82,14 @@ class ManuscriptPage extends Component {
             height: "170%"
           };
         return (
-        <div>
+        <div className="backgrounds">
            <div className="container">
                 <div class="circle" onClick={() => this.handleClick(0)}>
-                        <img src={IMG_URI} alt="Image" style={mystyle1}/> 
+                        <img src={IMG_URI_1} alt="Image" style={mystyle1}/> 
                         <div class="centered">What is BnF Ms. Fr. 640?</div>
                 </div>
             </div>
-            <div>{this.popUp0()}</div>
+            <div>{this.popUp()}</div>
         </div>
         );
     }
